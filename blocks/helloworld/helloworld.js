be74cf453f82afa25fae4a50464006f64b5b725f
Surabhi Achar <surabhiachar@Surabhis-MacBook-Pro.local>
2024-11-14 21:26:40 +0530

    text added

---
+++ blocks/helloworld/helloworld.js
@@ -0,0 +1,7 @@
+//js
+export default function decorate(block) {
+    const greeting = document.createElement('div');
+    greeting.textContent = 'Hello World';
+    block.textContent = '';
+    block.appendChild(greeting);
+}
